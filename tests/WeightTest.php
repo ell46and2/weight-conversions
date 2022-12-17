b5819f77c3735c2223cd09bdc9e6f4bd87ef33e1
Elliot Alderton <elliotalderton@elliots-mbp-2.home>
2022-12-17 16:08:21 +0000

    Add stones conversion. Add tests.

---
+++ tests/WeightTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use EllAlderton\WeightConversions\Weight;
+
+it('can convert kilograms to pounds correctly', function () {
+    $pounds = Weight::kilograms(100)->toPounds();
+
+    expect($pounds)->toEqual(220.46);
+});
+
+it('can convert kilograms to stones correctly', function () {
+    $stones = Weight::kilograms(100)->toStones();
+
+    expect($stones)->toEqual(15.747);
+});
